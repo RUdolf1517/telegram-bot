5b793588a263cae0b5467c3805340c6be1cdc58c
RUdolf1517 <mermerovich@mail.ru>
2023-12-17 17:58:47 +0300

    added numerations of jokes

--- host.py
+++ host.py
@@ -3,6 +3,16 @@ import random
 
 bot = telebot.TeleBot("6926925203:AAGilRh7PlJK9oFUZjcq8NcvlK1mMLznVkA")
 
+with open('jokes.txt', 'r+') as file:
+    lines = file.readlines()
+    file.seek(0)  # Перемещаем указатель в начало файла
+
+    for i, line in enumerate(lines, 1):
+        if line.startswith(f"{i}:"):
+            line = line.split(":", 1)[1]  # Удаляем текущую нумерацию
+        file.write(f"{i}: {line}")
+
+    file.truncate()
 
 @bot.message_handler(commands=['joke', 'шутку'])
 def send_joke(message):
