2b637257f11b99e3a06b64b88c352f4247ddf886
RUdolf1517 <mermerovich@mail.ru>
2023-12-17 17:44:41 +0300

    test start bot

---
+++ host.py
@@ -0,0 +1,20 @@
+import telebot
+import random
+
+bot = telebot.TeleBot("6926925203:AAGilRh7PlJK9oFUZjcq8NcvlK1mMLznVkA")
+
+
+@bot.message_handler(commands=['joke'])
+def send_joke(message):
+    # Открываем файл с шутками
+    with open('jokes.txt', 'r', encoding='utf-8') as file:
+        jokes = file.readlines()
+
+    # Выбираем случайную шутку
+    joke = random.choice(jokes)
+
+    # Отправляем шутку пользователю
+    bot.reply_to(message, joke)
+
+print("start")
+bot.polling()
